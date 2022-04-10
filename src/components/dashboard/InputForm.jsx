a0f486afbac498f40b4b007a130382799e8d4e08
piyushpoudyal <72690317+piyushsh25@users.noreply.github.com>
2022-04-10 11:59:17 +0530

    refactor : to check the length of the task name &desc

--- src/components/dashboard/InputForm.jsx
+++ src/components/dashboard/InputForm.jsx
@@ -10,7 +10,7 @@ export const InputForm = () => {
                 <input value={state.description} placeholder="task description " className="input" onChange={((e) => dispatch({ type: "description", payload: e }))} />
                 <input type="number" value={state.time} placeholder="time {in minutes} " className="input" onChange={((e) => dispatch({ type: "time", payload: e }))} />
                 <div>* task name and description shoudld have minimum 10 and 20 characters respectively. </div>
-                <button disabled={(state.name.length >= 10 && state.description.length >= 20 && state.time >= 1) ? !state.formInValid : state.formInValid} className="button" onClick={() => dispatch({ type: "submit" })}>Submit</button>
+                <button disabled={(state.name.split(" ").join("").length >= 10 && state.description.split(" ").join("").length >= 20 && state.time >= 1) ? !state.formInValid : state.formInValid} className="button" onClick={() => dispatch({ type: "submit" })}>Submit</button>
             </div>
         }
     </div>
